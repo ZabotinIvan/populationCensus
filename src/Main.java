9cda9777afd4f16c8d770086d812658a9a4f7c93
Ivan Zabotin <I.Y.Zabotin@mail.ru>
2022-09-26 13:49:09 +0300

    Внес правки

--- src/Main.java
+++ src/Main.java
@@ -12,27 +12,31 @@ public class Main {
                     families.get(new Random().nextInt(families.size())),
                     new Random().nextInt(100),
                     Sex.values()[new Random().nextInt(Sex.values().length)],
-                    Education.values()[new Random().nextInt(Education.values().length)])
-            );
-            persons.stream()
-                    .filter(x -> x.getAge() < 18)
-                    .count();
-            persons.stream()
-                    .filter(x -> x.getSex() == Sex.MAN)
-                    .filter(x -> x.getAge() > 18 && x.getAge() < 27)
-                    .map(Person::getFamily)
-                    .collect(Collectors.toList());
-            persons.stream()
-                    .filter(x -> x.getEducation() == Education.HIGHER)
-                    .filter(x -> (x.getSex() == Sex.WOMAN && x.getAge() > 18 && x.getAge() < 60)
-                            || (x.getSex() == Sex.MAN && x.getAge() > 18 && x.getAge() < 65))
-                    .sorted(Comparator.comparing(Person::getFamily))
-                    .collect(Collectors.toList());
-
-
-
-
+                    Education.values()[new Random().nextInt(Education.values().length)]));
         }
+        List<Long> minors = Collections.singletonList(persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count());
+        List<String> army = persons.stream()
+                .filter(x -> x.getSex() == Sex.MAN)
+                .filter(x -> x.getAge() > 18 && x.getAge() < 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        List<String> workPerson = persons.stream()
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(x -> (x.getSex() == Sex.WOMAN && x.getAge() > 18 && x.getAge() < 60)
+                            || (x.getSex() == Sex.MAN && x.getAge() > 18 && x.getAge() < 65))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        System.out.println(minors);
+        System.out.println(army);
+        System.out.println(workPerson);
+
+
+
+
     }
 
 }
